fbbc6901b56b031febc65d9d71d54b74a2bcf871
amandasjostrom <amandasjostrom@hotmail.com>
2019-12-12 22:32:37 +0100

    day4 part 1

---
+++ src/main/kotlin/se/amandasjostrom/aoc/nineteen/Day4.kt
@@ -0,0 +1,59 @@
+package se.amandasjostrom.aoc.nineteen
+
+import java.util.HashMap
+
+class Day4 {
+
+    fun partOne(start: Int, stop: Int) {
+        var current: Int = start
+        var count: Int = 0
+        while (current <= stop) {
+            println(current)
+            val currentAsArray = current.toString().toCharArray()
+            if (isIncreasing(currentAsArray) && hasDouble(currentAsArray)) {
+                count++
+            }
+            current++
+        }
+        println("day 4 part 1 result: $count")
+
+    }
+
+    private fun hasDouble(arr: CharArray): Boolean {
+        for (i in 0 until arr.size-1) {
+            if (arr[i] == arr[i + 1]) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun isIncreasing(arr: CharArray): Boolean {
+        for (i in 0 until arr.size-1) {
+            if (arr[i + 1] < arr[i]) {
+                return false
+            }
+        }
+        return true
+
+    }
+
+    private fun hasTrueDouble(arr: CharArray): Boolean {
+        val numberToCount = HashMap<Char, Int>()
+        for (i in arr.indices) {
+            val number = arr[i]
+            val currentCount = numberToCount[number]
+            if (currentCount == null) {
+                numberToCount[number] = 1
+            } else {
+                numberToCount[number] = currentCount + 1
+            }
+        }
+        for (count in numberToCount.values) {
+            if (count == 2) {
+                return true
+            }
+        }
+        return false
+    }
+}
---
+++ src/test/kotlin/se/amandasjostrom/aoc/nineteen/Day4Test.kt
@@ -0,0 +1,12 @@
+package se.amandasjostrom.aoc.nineteen
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class Day4Test{
+
+    @Test
+    fun name() {
+        Day4().partOne(134792,675810)
+    }
+}
