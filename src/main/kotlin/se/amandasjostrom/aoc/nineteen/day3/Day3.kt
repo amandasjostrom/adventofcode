1189d6e038ab25affcca82388e5e6daeb9efbe51
amandasjostrom <amandasjostrom@hotmail.com>
2019-12-14 14:38:04 +0100

    day 3 part 2

--- src/main/kotlin/se/amandasjostrom/aoc/nineteen/day3/Day3.kt
+++ src/main/kotlin/se/amandasjostrom/aoc/nineteen/day3/Day3.kt
@@ -5,22 +5,34 @@ import java.lang.Math.abs
 class Day3 {
 
     fun partOne(input: String): Int {
+        val (map, centralPort) = plotWiresOnMap(input)
+        return map.findDistanceToIntersectionClosestToCentralPort(centralPort)
+    }
+
+    fun partTwo(input: String): Int {
+        val (map, centralPort) = plotWiresOnMap(input)
+        return map.fewestCombinedStepsNeededToReachIntersection(centralPort)
+    }
+
+    private fun plotWiresOnMap(input: String): Pair<WireMap, Position> {
         val instructions = input.split('\n')
         val map = WireMap()
         val centralPort = Position(0, 0)
         putWireOnWireMap(instructions[0], "wire0", map, centralPort)
         putWireOnWireMap(instructions[1], "wire1", map, centralPort)
-        return map.findDistanceToIntersectionClosestToCentralPort(centralPort)
+        return Pair(map, centralPort)
     }
 
     private fun putWireOnWireMap(pathForWire: String, wireId: String, map: WireMap, startPosition: Position) {
         val instructions = pathForWire.split(',')
         var currentPosition = startPosition
+        var totalDistance = 0
 
         for (instruction in instructions) {
             val direction = instruction[0]
             val distance = Integer.valueOf(instruction.substring(1))
             for (x in 1..distance) {
+                totalDistance+=1
                 when (direction) {
                     'L' -> {
                         currentPosition = Position(currentPosition.x - 1, currentPosition.y)
@@ -35,7 +47,7 @@ class Day3 {
                         currentPosition = Position(currentPosition.x, currentPosition.y - 1)
                     }
                 }
-                map.placeWireAtPosition(wireId, currentPosition)
+                map.placeWireAtPosition(wireId, currentPosition, totalDistance)
             }
         }
     }
@@ -43,14 +55,13 @@ class Day3 {
 
 private class WireMap {
 
-    private val map = emptyMap<Position, MutableSet<String>>().toMutableMap()
+    private val map = emptyMap<Position, MutableMap<String, Int>>().toMutableMap()
 
-    fun placeWireAtPosition(wireId: String, position: Position) {
+    fun placeWireAtPosition(wireId: String, position: Position, distance: Int) {
         if (map.containsKey(position)) {
-            val set = map[position]
-            set!!.add(wireId)
+            map[position]!!.putIfAbsent(wireId, distance)
         } else {
-            map[position] = mutableSetOf(wireId)
+            map[position] = mutableMapOf(Pair(wireId, distance))
         }
     }
 
@@ -63,6 +74,17 @@ private class WireMap {
 
     private fun manhattanDistanceBetween(pos1: Position, pos2: Position): Int {
         return abs(pos1.x - pos2.x) + abs(pos1.y - pos2.y)
+    }
+
+    fun fewestCombinedStepsNeededToReachIntersection(centralPort: Position): Int {
+        return map.filter { (k, v) -> v.size > 1 && k != centralPort }
+                .map { (k, _) -> combinedSteps(k) }
+                .sorted()
+                .first()
+    }
+
+    private fun combinedSteps(position: Position): Int {
+        return map[position]!!.map { (_,v) -> v }.sum()
     }
 }
 
